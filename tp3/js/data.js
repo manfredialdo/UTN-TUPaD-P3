68049b22e40d94ce69148a17490db1672095571a
manfredialdo <manfredialdo.1979@gmail.com>
2026-04-08 21:20:51 +0000

    ultimos arreglos

--- tp3/js/data.js
+++ tp3/js/data.js
@@ -6,6 +6,7 @@ const menu = [
         descripcion: "un clásico argentino: pan, chorizo de cerdo y chimi.",
         precio: "1500",
         imagen: "assets/chori.jpg",
+		categoria: "comida",
         alt: "el choripan es monton de tripas de chancho todas condimentadas picadas metidas dentro de otra tripa y despues todas cocidas .. caliente salido de la parri todo eso metido dentro de un pan blanco... si te da la gana hay aderezo...",
         title: "el chori de la casa"
       },
@@ -15,6 +16,7 @@ const menu = [
         descripcion: "Empanada salteña de carne cortada a cuchillo, frita y a leña.",
         precio: "800",
         imagen: "assets/empanada.jpg",
+		categoria: "comida",
         alt: "la empanada es ese tesoro de masa dorada, horneada o frita en grasa caliente hasta que burbujea... y q adentro guarda un corazón con carne cortada a cuchillo, mucha cebolla rehogada, morron y un juguito que se te corre por entre el brazo y si no abrís bien las piernas podes mancharte... es el resumen de la felicidad envuelto en un repulgue artesanal...",
         title: "la empanada de la casa, con carne cortada a cuchillo"
       },
@@ -24,6 +26,7 @@ const menu = [
         descripcion: "vino, soda y adentro.",
         precio: "2500",
         imagen: "assets/visoda.jpg",
+		categoria: "bebida",
         alt: "el vino con soda es ese tinto en caja, fuerte y con cuerpo, que se doma con el golpe de burbujas del sifón bien frío... es el refresco de los dioses del barrio, servido en vaso de vidrio grueso con un par de hielos que tintinean mientras esperás el asado... es un equilibrio entre la uva madura y el gas que te raspa la garganta justo como tiene que ser...",
         title: "vino con soda, un clasico"
       }
